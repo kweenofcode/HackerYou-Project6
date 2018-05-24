d18b85a60761d2cec26fe075a220e551c731e64e
Becky Pollard <hello@beckypollard.com>
2018-05-24 15:23:54 -0400

    Added VQA logo

--- dev/scripts/LandingPage.js
+++ dev/scripts/LandingPage.js
@@ -6,26 +6,26 @@ import {
 
 const LandingPage = () => {
         return (
-          <main className="landingPage">
+          <section className="landingPage">
+          <a href="http://www.vqaontario.ca/Home"><img src="images/VQAlogo.png" alt="VQA Wines of Ontario Logo" className="VQAlogo"/></a>
               <div className="landingContent">
                 <h1>Wine ON, Dine ON</h1>
                 <p className="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. At magni voluptates ipsa nihil quae consectetur nobis, sequi vel fuga doloremque quidem quibusdam nisi praesentium dicta labore iste iusto placeat dolores neque cum? Sapiente eveniet sequi vel fuga doloremque rem vitae? Excepturi, id exercitationem nisi, soluta minima quisquam ducimus quae error, nulla beatae esse laboriosam!</p>
                 <form action="">
-                <div>
-                  <p>First, indicate any dietary restrictions:</p>
-                  <input type="checkbox" id="vegan"/><label htmlFor="vegan">vegan</label>
-                  <input type="checkbox" id="paleo"/><label htmlFor="paleo">paleo</label>
-                  <input type="checkbox" id="gluten"/><label htmlFor="gluten">Gluten-Free</label>
-                  <input type="checkbox" id="seafood"/><label htmlFor="seafood">Seafood-Free</label>
-                  <input type="checkbox" id="soy"/><label htmlFor="soy">Soy-Free</label>
-                  <input type="checkbox" id="dairy"/><label htmlFor="dairy">Diary-Free</label>
-                  <input type="checkbox" id="egg"/><label htmlFor="egg">Egg-Free</label>
-                  <input type="checkbox" id="treenut"/><label htmlFor="treenut">Tree-Nut-Free</label>
-                </div>
+                  <div>
+                    <input type="checkbox" id="vegan"/><label htmlFor="vegan">vegan</label>
+                    <input type="checkbox" id="paleo"/><label htmlFor="paleo">paleo</label>
+                    <input type="checkbox" id="gluten"/><label htmlFor="gluten">Gluten-Free</label>
+                    <input type="checkbox" id="seafood"/><label   htmlFor="seafood">Seafood-Free</label>
+                    <input type="checkbox" id="soy"/><label htmlFor="soy">Soy-Free</label>
+                    <input type="checkbox" id="dairy"/><label htmlFor="dairy">Diary-Free</label>
+                    <input type="checkbox" id="egg"/><label htmlFor="egg">Egg-Free</label>
+                    <input type="checkbox" id="treenut"/><label   htmlFor="treenut">Tree-Nut-Free</label>
+                  </div>
                   <Link to="/recipes"><input type="submit" value="Explore"/></Link>
                 </form>
               </div>
-          </main>
+          </section>
         )
 }
 
