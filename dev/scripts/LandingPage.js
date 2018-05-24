1568eff8d882b58e6c29b6192e7c5f91e4a8d74d
Becky Pollard <hello@beckypollard.com>
2018-05-24 17:10:45 -0400

    Style'n, smile'n...

--- dev/scripts/LandingPage.js
+++ dev/scripts/LandingPage.js
@@ -7,7 +7,6 @@ import {
 const LandingPage = () => {
         return (
           <section className="landingPage">
-          <a href="http://www.vqaontario.ca/Home"><img src="images/VQAlogo.png" alt="VQA Wines of Ontario Logo" className="VQAlogo"/></a>
               <div className="landingContent">
                 <h1>Wine ON, Dine ON</h1>
                 <p className="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. At magni voluptates ipsa nihil quae consectetur nobis, sequi vel fuga doloremque quidem quibusdam nisi praesentium dicta labore iste iusto placeat dolores neque cum? Sapiente eveniet sequi vel fuga doloremque rem vitae? Excepturi, id exercitationem nisi, soluta minima quisquam ducimus quae error, nulla beatae esse laboriosam!</p>
