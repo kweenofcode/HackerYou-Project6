36a759273d20f64d6e0624fc6408bea1fe59c5d4
Becky Pollard <hello@beckypollard.com>
2018-05-24 13:03:49 -0400

    Added some checkboxes, fiddling with styles

--- dev/scripts/LandingPage.js
+++ dev/scripts/LandingPage.js
@@ -8,10 +8,14 @@ const LandingPage = () => {
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At magni voluptates ipsa nihil quae consectetur nobis, sequi vel fuga doloremque quidem quibusdam nisi praesentium dicta labore iste iusto placeat dolores neque cum? Sapiente eveniet sequi vel fuga doloremque rem vitae? Excepturi, id exercitationem nisi, soluta minima quisquam ducimus quae error, nulla beatae esse laboriosam!</p>
                 <form action="">
                 <div>
-                    <input type="checkbox" id=""/><label for="">Lactose</label>
-                    <input type="checkbox" id=""/><label for="">Vegan</label>
-                    <input type="checkbox" id=""/><label for="">Vegetarian</label>
-                    <input type="checkbox" id="TEST"/><label for="TEST">Gluten-Free</label>
+                    <input type="checkbox" id="vegan"/><label htmlFor="vegan">vegan</label>
+                    <input type="checkbox" id="paleo"/><label htmlFor="paleo">paleo</label>
+                    <input type="checkbox" id="gluten"/><label htmlFor="gluten">Gluten-Free</label>
+                    <input type="checkbox" id="seafood"/><label htmlFor="seafood">Seafood-Free</label>
+                    <input type="checkbox" id="soy"/><label htmlFor="soy">Soy-Free</label>
+                    <input type="checkbox" id="dairy"/><label htmlFor="dairy">Diary-Free</label>
+                    <input type="checkbox" id="egg"/><label htmlFor="egg">Egg-Free</label>
+                    <input type="checkbox" id="treenut"/><label htmlFor="treenut">Tree-Nut-Free</label>
                 </div>
                 <input type="submit" value="Explore"/>
                 </form>
