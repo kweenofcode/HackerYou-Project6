8b2e1afc0dc49459151191c003083005d0ca7de2
Michael Yiu <onew1ng3d@gmail.com>
2018-05-25 15:10:35 -0400

    Wine component should be good now

--- dev/scripts/wine.js
+++ dev/scripts/wine.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Wine = (props) => {
     return (
         <div>
-            <img className="wineImage" src={this.state.oneWine.image_url}></img>
+            <img className="wineImage" src={props.image_url}></img>
             <h3>{props.name}</h3>
-            <p className="wineOrigin">{this.state.oneWine.origin}</p>
-            <p className="wineDesc">{this.state.oneWine.tasting_note}</p>
-            <p className="wineTags">{this.state.oneWine.secondary_category} | Alcohol Content: {this.state.oneWine.alcohol_content}| {this.state.oneWine.style}</p>
+            <p className="wineOrigin">{props.origin}</p>
+            <p className="wineDesc">{props.tasting_note}</p>
+            <p className="wineTags">{props.secondary_category} | Alcohol Content: {props.alcohol_content}| {props.style}</p>
         </div>
     )
 
